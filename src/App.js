fbda1bfd04ae0a6763add8f5d11d22bf221798db
harisw <hari14@mhs.if.its.ac.id>
2022-05-04 17:23:12 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import W3CWebSocket from 'websocket/lib/W3CWebSocket';
+
+const client = new W3CWebSocket('ws://localhost:3001/ws');
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1 className='text-3xl font-bold'>
+      Hello!
+    </h1>
   );
 }
 
